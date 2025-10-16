88de46642d54bb23f96876db7d4c9d91c02e482e
asael <148477747+asaelitop@users.noreply.github.com>
2025-10-16 06:18:51 +0000

    prueba 6

--- src/person.py
+++ src/person.py
@@ -7,8 +7,8 @@ class Person:
     """
     
     def __init__(self, first_name, last_name):
-        self.first_name = 'Asael'
-        self.last_name = 'Otero'
+        self.first_name = first_name
+        self.last_name = last_name
 
     def __str__(self):
         nombre_completo = f"{self.first_name} {self.last_name}"
